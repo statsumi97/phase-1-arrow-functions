334f8d8df9eae7d045bc2f6c9b00a2a4205329e5
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-11-02 16:13:37 -0400

    final product

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = (a, b) => a/b;
+divide(2000, 100);
+//
+const square = x => x * x;
+square(2);
+//
+const add = (a, b) => a + b;
+add(3, 4);
+
+/*it("has an arrow function called add", () => {
+    expect(add).to.exist
+  })
+
+  it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3
+    let b = 4
+    expect(add(a,b)).to.equal(a+b)
+  })
+  */
+ 
